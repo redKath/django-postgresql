e99533c884a358725952b5698d013ad6c58728af
Kathleen Jogno <kchan01412@gmail.com>
2026-02-09 22:41:05 +0800

    add files

---
+++ recipe_project/recipe/views.py
@@ -0,0 +1,44 @@
+from django.shortcuts import render, redirect
+from .forms import RecipeForm
+from .models import Recipe
+from django.contrib import messages
+
+# Create your views here.
+def add_recipe(request):
+	if request.method == 'POST':
+		form = RecipeForm(request.POST)
+		if form.is_valid():
+			form.save()
+			return redirect('list_recipes')
+	else:
+		form = RecipeForm()
+	return render(request, 'recipe/add_recipe.html', {'form': form})
+
+def list_recipes(request):
+	recipes = Recipe.objects.all()
+	return render(request, 'recipe/list_recipes.html', {'recipes': recipes})
+
+def update_recipe(request, recipe_id):
+	recipe = Recipe.objects.get(pk=recipe_id)
+	if request.method == 'POST':
+		form = RecipeForm(request.POST, instance=recipe)
+		if form.is_valid():
+			form.save()
+			return redirect('list_recipes')
+	else:
+		form = RecipeForm(instance=recipe)
+	return render(request, 'recipe/update_recipe.html', {'form': form})
+
+def delete_recipe(request, recipe_id):
+	recipe = Recipe.objects.get(pk=recipe_id)
+	recipe.delete()
+	messages.success(request, 'Recipe deleted successfully.')
+	return redirect('list_recipes')	
+
+def search_recipes(request):
+	query = request.GET.get('q')
+	if query:
+		results = Recipe.objects.filter(name__icontains=query) | Recipe.objects.filter(ingredients__icontains=query) | Recipe.objects.filter(instructions__icontains=query)
+	else:
+		results = Recipe.objects.none()
+	return render(request, 'recipe/search_recipes.html', {'results': results, 'query': query})
